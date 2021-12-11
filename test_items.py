20dee5f40e7980069be999236ab1795e75050e8f
tanik1408 <tanik1408>
2021-12-11 19:29:03 +0300

    remove warnings

--- test_items.py
+++ test_items.py
@@ -1,5 +1,7 @@
+from selenium.webdriver.common.by import By
+
 def test_button_add_product_is_displayed(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    button = browser.find_elements(By.CSS_SELECTOR,".btn-add-to-basket")
     assert len(button) > 0, 'button not found'
