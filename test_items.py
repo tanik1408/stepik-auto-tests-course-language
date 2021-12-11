251c949f5f19a5f5f08c63a06d0ca6b8c541dc10
tanik1408 <tanik1408>
2021-12-11 18:15:41 +0300

    rename test function

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-def test_button_add_product(browser):
+def test_button_add_product_is_displayed(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button = browser.find_elements_by_css_selector(".btn-add-to-basket")
