0226f8d0a15f3d2e0cd22898931927dcf986f39e
tanik1408 <tanik1408>
2021-12-11 17:44:02 +0300

    add a test to find a button (change implementation)

--- conftest.py
+++ conftest.py
@@ -1,22 +1,18 @@
 import pytest
 from selenium import webdriver
-
-language = None
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language: ru, en, es, fr and etc")
+                     help="Choose language: ru, en, es, fr and ... etc")
 
 @pytest.fixture(scope="function")
-def browser():
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print("\nstart browser ..")
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-@pytest.fixture
-def language_url(request):
-    global language
-    language = request.config.getoption("language")
-    return language
--- test_items.py
+++ test_items.py
@@ -1,5 +1,5 @@
-def test_button_add_product(browser, language_url):
-    link = f"http://selenium1py.pythonanywhere.com/{language_url}/catalogue/coders-at-work_207/"
+def test_button_add_product(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button = browser.find_elements_by_css_selector(".btn-add-to-basket")
     assert len(button) > 0, 'button not found'
